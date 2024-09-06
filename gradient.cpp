1ac1d4ce39190d8da512f8996412bc642857c47f
shahn63 <132188355+shahn63@users.noreply.github.com>
2024-09-06 23:02:45 +0900

    Update gradient.cpp

--- gradient.cpp
+++ gradient.cpp
@@ -4,13 +4,11 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 NumericMatrix gradient(NumericMatrix mat1, NumericMatrix mat2, NumericMatrix mats, int n1, int n2) {
 
-  // 결과를 저장할 행렬
-	NumericMatrix value1(n1,2);
+  	NumericMatrix value1(n1,2);
 	NumericMatrix mat3(n1,2);
 	NumericMatrix grads1(n1,n2), grads2(n1,n2), grads(n1,n2+n2);
 
-  // 벡터의 원소를 제곱하고 더하기
-	for (int jj = 0; jj < n2; jj++){
+ 	for (int jj = 0; jj < n2; jj++){
 		mat3(_,0)=mat1(_,jj);
 		mat3(_,1)=mat2(_,jj);
 		for (int i = 0; i < n1; i++){
